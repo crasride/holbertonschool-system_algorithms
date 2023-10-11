8465847f77924950c59eb91a3316b35cb958d670
crasride <jose.dfernandez@yahoo.fr>
2023-10-11 13:29:43 +0200

    task1

---
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -0,0 +1,28 @@
+#include "rb_trees.h"
+
+int is_valid_rbt(const rb_tree_t *tree, int black_count, rb_color_t prev_color)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (prev_color == RED && tree->color == RED)
+		return (0);
+
+	if (tree->color == BLACK)
+		black_count++;
+
+	return (is_valid_rbt(tree->left, black_count, tree->color) &&
+						is_valid_rbt(tree->right, black_count, tree->color));
+}
+
+int rb_tree_is_valid(const rb_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->color !=BLACK)
+		return (0);
+
+	return is_valid_rbt(tree, 0, BLACK);
+}
+
--- red_black_tree/rb_trees.h
+++ red_black_tree/rb_trees.h
@@ -37,5 +37,7 @@ typedef struct rb_tree_s
 } rb_tree_t;
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+int rb_tree_is_valid(const rb_tree_t *tree);
+int is_valid_rbt(const rb_tree_t *tree, int black_count, rb_color_t prev_color);
 
 #endif /* _RB_TREES_H_ */

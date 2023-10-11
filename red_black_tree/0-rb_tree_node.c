acfd4b82661f4f2a3e5a6cdb887ff73729d44a36
crasride <jose.dfernandez@yahoo.fr>
2023-10-11 11:16:22 +0200

    task0

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -1,6 +1,13 @@
 #include "rb_trees.h"
 
 
+/**
+* rb_tree_node - Creates a Red-Black Tree node
+* @parent: Pointer to the parent node of the node to create
+* @value: The value to put in the new node
+* @color: The color of the node (RED, BLACK, or DOUBLE_BLACK)
+* Return: A pointer to the new node, or NULL on failure
+*/
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 	rb_tree_t *new_node;

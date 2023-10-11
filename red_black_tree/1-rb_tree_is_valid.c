509c80d64e0989e7bfa69abc855272c8e1435002
crasride <jose.dfernandez@yahoo.fr>
2023-10-11 15:27:50 +0200

    betty fix

--- red_black_tree/1-rb_tree_is_valid.c
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -1,5 +1,12 @@
 #include "rb_trees.h"
 
+/**
+* is_valid_rbt - Checks if a subtree is a valid Red-Black Tree.
+* @tree: Pointer to the root node of the subtree to check.
+* @black_count: Number of black nodes in the path from the root to this node.
+* @prev_color: Color of the parent node.
+* Return: 1 if it's a valid Red-Black Tree, 0 otherwise.
+*/
 int is_valid_rbt(const rb_tree_t *tree, int black_count, rb_color_t prev_color)
 {
 	if (tree == NULL)
@@ -18,14 +25,18 @@ int is_valid_rbt(const rb_tree_t *tree, int black_count, rb_color_t prev_color)
 			is_valid_rbt(tree->right, black_count, tree->color));
 }
 
+/**
+* rb_tree_is_valid - Checks if a tree is a valid Red-Black Tree.
+* @tree: Pointer to the root node of the tree to check.
+* Return: 1 if it's a valid Red-Black Tree, 0 otherwise.
+*/
 int rb_tree_is_valid(const rb_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	if (tree->color !=BLACK)
+	if (tree->color != BLACK)
 		return (0);
 
-	return is_valid_rbt(tree, 0, BLACK);
+	return (is_valid_rbt(tree, 0, BLACK));
 }
-

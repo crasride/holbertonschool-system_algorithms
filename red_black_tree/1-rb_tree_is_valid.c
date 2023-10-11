9d64438184ac3e684239e750cade0fb0e2599369
crasride <jose.dfernandez@yahoo.fr>
2023-10-11 15:21:03 +0200

    case  Not valid RB tree

--- red_black_tree/1-rb_tree_is_valid.c
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -8,11 +8,14 @@ int is_valid_rbt(const rb_tree_t *tree, int black_count, rb_color_t prev_color)
 	if (prev_color == RED && tree->color == RED)
 		return (0);
 
+	if (tree->color == BLACK && prev_color == RED)
+		return (0);
+
 	if (tree->color == BLACK)
 		black_count++;
 
 	return (is_valid_rbt(tree->left, black_count, tree->color) &&
-						is_valid_rbt(tree->right, black_count, tree->color));
+			is_valid_rbt(tree->right, black_count, tree->color));
 }
 
 int rb_tree_is_valid(const rb_tree_t *tree)

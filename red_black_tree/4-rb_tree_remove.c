d2796d468628481582ea5ad95d2c51eee6bab970
crasride <jose.dfernandez@yahoo.fr>
2023-10-12 16:58:21 +0200

    task 4

--- red_black_tree/4-rb_tree_remove.c
+++ red_black_tree/4-rb_tree_remove.c
@@ -3,5 +3,31 @@
 
 rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
 {
-	return (root);
+	if (root == NULL)
+		return root;
+
+
+	if (n < root->n)
+	{
+		root->left = rb_tree_remove(root->left, n);
+	}
+	else if (n > root->n)
+	{
+		root->right = rb_tree_remove(root->right, n);
+	}
+	else
+	{
+
+		if (root->left == NULL || root->right == NULL)
+		{
+			rb_tree_t *temp = (root->left != NULL) ? root->left : root->right;
+			free(root);
+			return temp;
+		}
+
+		rb_tree_t *temp = find_min(root->right);
+		root->right = rb_tree_remove(root->right, temp->n);
+	}
+
+	return root;
 }

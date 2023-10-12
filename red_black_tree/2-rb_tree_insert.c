f310ca28e3741900c9e41ea59038a40b622ff400
crasride <jose.dfernandez@yahoo.fr>
2023-10-12 16:43:50 +0200

    update

--- red_black_tree/2-rb_tree_insert.c
+++ red_black_tree/2-rb_tree_insert.c
@@ -158,3 +158,70 @@ rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value)
 
 	return (new_node);
 }
+
+/**
+ * rotate_left - Perform a left rotation on the given nodes.
+ * @tree: Double pointer to the root node of the tree.
+ * @parent: Pointer to the parent node to rotate.
+ */
+void rotate_left(rb_tree_t **tree, rb_tree_t *parent)
+{
+	rb_tree_t *right_child = parent->right;
+
+	parent->right = right_child->left;
+
+	if (parent->right)
+		parent->right->parent = parent;
+
+	right_child->parent = parent->parent;
+
+	if (!parent->parent)
+		*tree = right_child;
+	else if (parent == parent->parent->left)
+		parent->parent->left = right_child;
+	else
+		parent->parent->right = right_child;
+
+	right_child->left = parent;
+	parent->parent = right_child;
+}
+
+/**
+ * rotate_right - Perform a right rotation on the given nodes.
+ * @tree: Double pointer to the root node of the tree.
+ * @grandparent: Pointer to the grandparent node to rotate.
+ */
+void rotate_right(rb_tree_t **tree, rb_tree_t *grandparent)
+{
+	rb_tree_t *parent = grandparent->left;
+
+	grandparent->left = parent->right;
+
+	if (parent->right)
+		parent->right->parent = grandparent;
+
+	parent->parent = grandparent->parent;
+
+	if (!grandparent->parent)
+		*tree = parent;
+	else if (grandparent == grandparent->parent->left)
+		grandparent->parent->left = parent;
+	else
+		grandparent->parent->right = parent;
+
+	parent->right = grandparent;
+	grandparent->parent = parent;
+}
+
+/**
+ * swap_colors - Swap the colors of two nodes.
+ * @parent: Pointer to the first node.
+ * @grandparent: Pointer to the second node.
+ */
+void swap_colors(rb_tree_t *parent, rb_tree_t *grandparent)
+{
+	rb_color_t temp = parent->color;
+
+	parent->color = grandparent->color;
+	grandparent->color = temp;
+}

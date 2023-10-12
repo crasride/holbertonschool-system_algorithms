9c8296e812ce418de8e82887e57509dcaa0025c7
crasride <jose.dfernandez@yahoo.fr>
2023-10-12 17:21:04 +0200

    task 4

--- red_black_tree/4-rb_tree_remove.c
+++ red_black_tree/4-rb_tree_remove.c
@@ -2,20 +2,5 @@
 
 rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
 {
-	rb_tree_t *cur = root;
 
-	/* find that guy to kick off */
-	while (cur)
-	{
-		if (cur->n == n)
-			break;
-		else if (cur->n < n)
-			cur = cur->left;
-		else
-			cur = cur->right;
-	}
-
-	if (!cur)
-		return (root);
-	return (root);
 }

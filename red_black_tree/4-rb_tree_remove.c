8c88e5a7d343bee08ca1939b293599505196af61
crasride <jose.dfernandez@yahoo.fr>
2023-10-12 17:00:16 +0200

    task 4

--- red_black_tree/4-rb_tree_remove.c
+++ red_black_tree/4-rb_tree_remove.c
@@ -1,11 +1,15 @@
 #include "rb_trees.h"
 
-
+/**
+ * rb_tree_remove - Removes a node from a Red-Black tree.
+ * @root: Pointer to the root node of the tree.
+ * @n: The value to search and remove from the tree.
+ * Return: The pointer to the new root of the tree after deletion.
+ */
 rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
 {
 	if (root == NULL)
 		return root;
-
 
 	if (n < root->n)
 	{
@@ -17,15 +21,21 @@ rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
 	}
 	else
 	{
-
-		if (root->left == NULL || root->right == NULL)
+		if (root->left == NULL)
 		{
-			rb_tree_t *temp = (root->left != NULL) ? root->left : root->right;
+			rb_tree_t *temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL)
+		{
+			rb_tree_t *temp = root->left;
 			free(root);
 			return temp;
 		}
 
 		rb_tree_t *temp = find_min(root->right);
+		root->n = temp->n;
 		root->right = rb_tree_remove(root->right, temp->n);
 	}
 

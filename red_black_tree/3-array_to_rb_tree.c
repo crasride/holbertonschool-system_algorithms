e9fb64a2a36e425dcb3a1fb7293a955596830641
crasride <jose.dfernandez@yahoo.fr>
2023-10-12 13:35:53 +0200

    add description function

--- red_black_tree/3-array_to_rb_tree.c
+++ red_black_tree/3-array_to_rb_tree.c
@@ -1,5 +1,11 @@
 #include "rb_trees.h"
 
+/**
+* array_to_rb_tree - Builds a Red-Black Tree from an array.
+* @array: Pointer to the first element of the array to be converted.
+* @size: The number of elements in the array.
+* Return: A pointer to the root node of the created Red-Black tree, or NULL.
+*/
 rb_tree_t *array_to_rb_tree(int *array, size_t size)
 {
 	/* Initializes the root pointer as NULL */

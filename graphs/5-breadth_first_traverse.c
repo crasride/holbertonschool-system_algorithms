88e902c65b064ab84945a55af5dd1acb64907e2d
crasride <jose.dfernandez@yahoo.fr>
2023-11-28 09:33:52 +0100

    add description y comment

--- graphs/5-breadth_first_traverse.c
+++ graphs/5-breadth_first_traverse.c
@@ -4,6 +4,16 @@
 #include <string.h>
 
 
+/**
+* bfs_traverse_from_vertex - Performs a breadth-first traversal on a graph
+* starting from a given vertex.
+* @graph: Pointer to the graph to traverse.
+* @start_vertex: Pointer to the starting vertex for the traversal.
+* @visited: Array to keep track of visited vertices.
+* @action: Pointer to the action function called for each visited vertex.
+* @max_depth: Pointer to the value of the maximum depth reached during the
+* traversal.
+*/
 void bfs_traverse_from_vertex(const graph_t *graph,
 									vertex_t *start_vertex,
 									int *visited,
@@ -22,7 +32,7 @@ void bfs_traverse_from_vertex(const graph_t *graph,
 	front = 0, rear = 0;
 
 	queue[rear] = start_vertex;
-	visited[start_vertex->index] = 1;
+	visited[start_vertex->index] = 1; /* Mark the starting vertex as visited */
 
 	while (front <= rear)
 	{
@@ -30,28 +40,34 @@ void bfs_traverse_from_vertex(const graph_t *graph,
 		depth = visited[current_vertex->index] - 1;
 
 		if (depth > *max_depth)
-			*max_depth = depth;
+			*max_depth = depth; /* Update the maximum depth if needed */
 
 		action(current_vertex, depth);
 
 		edge = current_vertex->edges;
-
+		/* Iterate through the edges of the current vertex */
 		while (edge != NULL)
 		{
 			if (!visited[edge->dest->index])
 			{
-				queue[++rear] = edge->dest;
+				queue[++rear] = edge->dest;/* Enqueue the destination vertex */
+				/* Mark the destination as visited */
 				visited[edge->dest->index] = depth + 2;
 			}
 			edge = edge->next;
 		}
-		front++;
+		front++; /* Dequeue the current vertex */
 	}
 
 	free(queue);
 }
 
-
+/**
+* breadth_first_traverse - Performs a breadth-first traversal on a graph.
+* @graph: Pointer to the graph to traverse.
+* @action: Pointer to the action function called for each visited vertex.
+* Return: The maximum depth reached during the traversal or 0 on failure.
+*/
 size_t breadth_first_traverse(const graph_t *graph,
 								void (*action)(const vertex_t *v, size_t depth))
 {
@@ -75,7 +91,7 @@ size_t breadth_first_traverse(const graph_t *graph,
 		free(visited);
 		return (0);
 	}
-
+	/* Traverse the graph starting from each unvisited vertex */
 	while (current_vertex != NULL)
 	{
 		if (!visited[current_vertex->index])

2a787f4eba537e8dfcfd6decbc8d961d224dd0a0
crasride <jose.dfernandez@yahoo.fr>
2023-12-18 09:37:20 +0100

    add files for test

---
+++ huffman_coding/huffman.h
@@ -0,0 +1,23 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "heap/heap.h"
+
+/**
+* struct symbol_s - Stores a char and its associated frequency
+*
+* @data: The character
+* @freq: The associated frequency
+*/
+typedef struct symbol_s
+{
+	char data;
+	size_t freq;
+} symbol_t;
+
+/* Functions*/
+
+
+#endif /* HUFFMAN_H */

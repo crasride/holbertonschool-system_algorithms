14fe3b71d9ffa597d2f83041a6b0d2adf6725066
crasride <jose.dfernandez@yahoo.fr>
2023-12-19 14:57:16 +0100

    fix c90

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -10,18 +10,21 @@
 */
 int huffman_extract_and_insert(heap_t *priority_queue)
 {
+	binary_tree_node_t *node1 = NULL, *node2 = NULL, *new_node = NULL;
+	symbol_t *symbol1 = NULL, *symbol2 = NULL, *new_symbol = NULL;
+
 	if (priority_queue == NULL || priority_queue->size < 2)
 		return (0);
 	/* Extract the two nodes with the lowest frequencies */
-	binary_tree_node_t *node1 = heap_extract(priority_queue);
-	binary_tree_node_t *node2 = heap_extract(priority_queue);
+	node1 = heap_extract(priority_queue);
+	node2 = heap_extract(priority_queue);
 
 	if (node1 == NULL || node2 == NULL)
 		return (0);
 	/* Convert the void * to symbol_t * before accessing the 'freq' member */
-	symbol_t *symbol1 = (symbol_t *)node1->data;
-	symbol_t *symbol2 = (symbol_t *)node2->data;
-	symbol_t *new_symbol = symbol_create(-1, symbol1->freq + symbol2->freq);
+	symbol1 = (symbol_t *)node1->data;
+	symbol2 = (symbol_t *)node2->data;
+	new_symbol = symbol_create(-1, symbol1->freq + symbol2->freq);
 
 	if (new_symbol == NULL)
 	{
@@ -32,7 +35,7 @@ int huffman_extract_and_insert(heap_t *priority_queue)
 		return (0);
 	}
 	/* Create a new node with the new symbol */
-	binary_tree_node_t *new_node = binary_tree_node(NULL, new_symbol);
+	new_node = binary_tree_node(NULL, new_symbol);
 
 	if (new_node == NULL)
 	{

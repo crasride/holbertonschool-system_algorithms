fd39ea3d85aeb39aef6338c8eebf93369ee4e169
crasride <jose.dfernandez@yahoo.fr>
2023-12-19 16:04:54 +0100

    task 8

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -1,44 +1,27 @@
-#include "heap.h"
 #include "huffman.h"
 
 /**
-* huffman_tree - Builds the Huffman tree.
-* @data: Array of characters.
-* @freq: Array containing associated frequencies.
-* @size: Size of the arrays.
+* huffman_tree - build a Huffman tree
+* @data: array of characters
+* @freq: array of associated character frequencies
+* @size: size of both arrays
 *
-* Return: Pointer to the root node of the Huffman tree, or NULL on failure.
+* Return: pointer to root node of Huffman tree or NULL on failure
 */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
-	heap_t *priority_queue;
-	binary_tree_node_t *root;
+	heap_t *prio_q;
+	binary_tree_node_t *h_tree;
 
-	if (data == NULL || freq == NULL || size == 0)
+	if (!data || !freq || !size)
 		return (NULL);
-
-	priority_queue = huffman_priority_queue(data, freq, size);
-	if (priority_queue == NULL)
+	prio_q = huffman_priority_queue(data, freq, size);
+	if (!prio_q)
 		return (NULL);
-
-	while (priority_queue->size > 1)
-	{
-		if (!huffman_extract_and_insert(priority_queue))
-		{
-			heap_delete(priority_queue, free_symbol);
-			return (NULL);
-		}
-	}
-
-	root = heap_extract(priority_queue);
-	if (root == NULL)
-	{
-		heap_delete(priority_queue, free_symbol);
-		return (NULL);
-	}
-
-	heap_delete(priority_queue, NULL);
-
-	return (root);
+	while (huffman_extract_and_insert(prio_q))
+		;
+	h_tree = prio_q->root->data;
+	free(prio_q->root);
+	free(prio_q);
+	return (h_tree);
 }
-

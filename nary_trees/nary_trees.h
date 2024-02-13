3b957bddc929d120d6cb03e5494fe487cd61633f
crasride <jose.dfernandez@yahoo.fr>
2024-02-13 09:03:25 +0100

    functions

---
+++ nary_trees/nary_trees.h
@@ -0,0 +1,36 @@
+#ifndef __N_ARY_TREES__
+#define __N_ARY_TREES__
+
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* struct nary_tree_s - N-ary tree node structure
+*
+* @content: Content of the node
+* @parent: Pointer to the parent node
+* @nb_children: Number of children
+* @children: Pointer to the head node of the children linked list
+* @next: Pointer to the next child in the parent's children linked list
+*/
+typedef struct nary_tree_s
+{
+	char            *content;
+	struct nary_tree_s  *parent;
+	size_t          nb_children;
+	struct nary_tree_s  *children;
+	struct nary_tree_s  *next;
+} nary_tree_t;
+
+/* Functions Taks*/
+nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str);
+void nary_tree_delete(nary_tree_t *tree);
+size_t nary_tree_traverse(nary_tree_t const *root,
+						void (*action)(nary_tree_t const *node, size_t depth));
+size_t nary_tree_diameter(nary_tree_t const *root);
+int path_exists(nary_tree_t const *root, char const * const *path);
+
+#endif /* __N_ARY_TREES__ */
